e4e88e900bd11153b9d0e451ba4b39d1eb3ffc97
Annamalai Maniveerappan <121749563+sabersmash1412@users.noreply.github.com>
2025-08-23 17:57:07 +0800

    Implement feature add and list

--- src/main/java/Mani.java
+++ src/main/java/Mani.java
@@ -4,13 +4,25 @@ public class Mani {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String[] memory = new String[100];
+        int count = 0;
+
         System.out.println("Hello! I'm Mani");
         System.out.println("What can I do for you?");
 
         String text = sc.nextLine();
 
         while (!text.equals("bye")) {
-            System.out.println(text);
+            if (text.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + memory[i]);
+                }
+                text = sc.nextLine();
+                continue;
+            }
+            System.out.println("added: " + text);
+            memory[count] = text;
+            count++;
             text = sc.nextLine();
         }
 
