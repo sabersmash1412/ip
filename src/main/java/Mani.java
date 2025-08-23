68dd33a208e2b7d70810c13196c33b82eebbdfbf
Annamalai Maniveerappan <121749563+sabersmash1412@users.noreply.github.com>
2025-08-23 17:41:50 +0800

    Implement echo and exit feature

--- src/main/java/Mani.java
+++ src/main/java/Mani.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Mani {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello! I'm Mani");
         System.out.println("What can I do for you?");
+
+        String text = sc.nextLine();
+
+        while (!text.equals("bye")) {
+            System.out.println(text);
+            text = sc.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
     }
 }
