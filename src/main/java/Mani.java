b85ffcff2e808c7539292c512154e9dbd5ebdd15
Annamalai Maniveerappan <121749563+sabersmash1412@users.noreply.github.com>
2025-08-23 17:19:49 +0800

    Implement Mani chatbot skeleton with greeting and exit

---
+++ src/main/java/Mani.java
@@ -0,0 +1,7 @@
+public class Mani {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Mani");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
